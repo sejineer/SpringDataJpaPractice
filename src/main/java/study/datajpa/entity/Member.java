d27593f1cbf8920d296ac3f41589863baddaf723
박세진 <jinpark99@naver.com>
2023-04-15 03:35:15 +0900

    [FEAT]: 프로젝트 환경설정

--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -13,4 +13,12 @@ public class Member {
     @Id @GeneratedValue
     private Long id;
     private String username;
+
+    protected Member() {
+    }
+
+    public Member(String username) {
+        this.username = username;
+    }
+
 }
